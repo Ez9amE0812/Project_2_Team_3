afc194af1e9d26ecdbd7c94632f33bb11f117d85
Toan Le <83160967+QuachTinh0812@users.noreply.github.com>
2021-05-27 12:35:25 +0700

    add tilemap, camera follow player

--- DieAnotherDay/Assets/Script/Player.cs
+++ DieAnotherDay/Assets/Script/Player.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 3f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
 
     public int maxHealth = 100;
     public int currentHealth;
@@ -21,7 +24,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
     void TakeDamage(int damage)
